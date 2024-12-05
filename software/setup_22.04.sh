20d1545acb2e7adbd834029a7a11de17093cdfc7
Ian OHara <ianohara@gmail.com>
2024-12-05 09:43:56 -0800

    Add pytest

--- software/setup_22.04.sh
+++ software/setup_22.04.sh
@@ -32,7 +32,7 @@ mkdir cache
 # install libraries 
 pip3 install qtpy pyserial pandas imageio crc==1.3.0 lxml numpy tifffile scipy napari
 pip3 install opencv-python-headless opencv-contrib-python-headless
-pip3 install napari[all] scikit-image dask_image ome_zarr aicsimageio basicpy
+pip3 install napari[all] scikit-image dask_image ome_zarr aicsimageio basicpy pytest
 
 # install camera drivers
 cd "$DAHENG_CAMERA_DRIVER_ROOT"
