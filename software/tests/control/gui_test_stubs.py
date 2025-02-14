861c2a93f7150e4bf0ec1b8fc5fe4746852aea16
Ian OHara <ianohara@gmail.com>
2025-02-13 18:19:43 -0800

    fix: for both config editors, catch and alert on file permission errors

--- software/tests/control/gui_test_stubs.py
+++ software/tests/control/gui_test_stubs.py
@@ -1,5 +1,10 @@
 import os
 import pathlib
+from typing import Callable
+import time
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QDialog, QApplication
 
 import control.core.core
 import control.microcontroller
