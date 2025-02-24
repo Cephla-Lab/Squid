ee21f51ea8de4bb7200ce10fe6f18971a593ca1c
Ian OHara <ianohara@gmail.com>
2025-02-24 10:35:37 -0800

    close up the test widgets properly

--- software/tests/control/test_HighContentScreeningGui.py
+++ software/tests/control/test_HighContentScreeningGui.py
@@ -8,6 +8,8 @@ def test_create_simulated_hcs_with_or_without_piezo(qtbot):
     # the piezo objective.
     control._def.HAS_OBJECTIVE_PIEZO = True
     with_piezo = control.gui_hcs.HighContentScreeningGui(is_simulation=True)
+    qtbot.add_widget(with_piezo)
 
     control._def.HAS_OBJECTIVE_PIEZO = False
     without_piezo = control.gui_hcs.HighContentScreeningGui(is_simulation=True)
+    qtbot.add_widget(without_piezo)
