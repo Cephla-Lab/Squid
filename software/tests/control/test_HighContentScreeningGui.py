edee0719037f03e61bb3770477cc276e13673d75
Ian OHara <ianohara@gmail.com>
2025-03-03 18:05:32 -0800

    test

--- software/tests/control/test_HighContentScreeningGui.py
+++ software/tests/control/test_HighContentScreeningGui.py
@@ -1,3 +1,4 @@
+import tempfile
 import time
 
 import control._def
@@ -22,13 +23,25 @@ def test_create_and_acquire_with_or_without_laser_af(qtbot):
     without_laser_autofocus = control.gui_hcs.HighContentScreeningGui(is_simulation=True)
     qtbot.add_widget(without_laser_autofocus)
 
+    temp_dir = tempfile.mkdtemp()
+
+    without_laser_autofocus.multipointController.set_base_path(temp_dir)
+    without_laser_autofocus.multipointController.start_new_experiment("test_experiment_no_laser_af")
     without_laser_autofocus.scanCoordinates.add_region("A1", 1, 2, 1, 10, shape="Square")
-    without_laser_autofocus.multipointController.set_selected_configurations("BF LED matrix full")
+    without_laser_autofocus.multipointController.set_selected_configurations(["BF LED matrix full"])
     without_laser_autofocus.multipointController.run_acquisition()
+
+    def acquisition_complete():
+        print("HJI")
+
+    without_laser_autofocus.multipointController.acquisitionFinished.connect(acquisition_complete)
 
     timeout_time = time.time() + 5
     while time.time() < timeout_time:
-        if not without_laser_autofocus.multipointController.acquisition_in_progress():
+        # TODO(imo): acquisition_in_progress doesn't return False, even when
+        if without_laser_autofocus.multipointController.acquisition_in_progress():
             time.sleep(0.1)
+        else:
+            break
 
     assert not without_laser_autofocus.multipointController.acquisition_in_progress()
